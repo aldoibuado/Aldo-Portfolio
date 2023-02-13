7fe00c81cd205b7492a9028f4918c1be5b082d22
aldoibuado <aldo.ibuado@gmail.com>
2023-02-13 10:38:19 -0800

    working on building the about section as well as adding a profile picture

--- myportfolio/src/components/About.js
+++ myportfolio/src/components/About.js
@@ -1,16 +1,44 @@
-function About () {
-    return (
-        <section id="about-me" class="about-me">
-        <h2>About</h2>
-        <p>
-          My name is Aldo and I am currently embarking on a journey learning web
-          development. I am currently working on some web applications that once
-          done, will go into my portfolio. What I enjoy about web development is
-          that it gives you the opportunity to be creative and make works of art
-          with code.
-        </p>
-      </section>
-    )
+function About() {
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            Hello, my name is Aldo.
+            <br className="hidden lg:inline-block" /> I enjoy learning to code
+            and building applications.
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            I am currently working on some web applications that once done, will
+            go into my portfolio. What I enjoy about web development is that it
+            gives you the opportunity to be creative and make works of art with
+            code.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./Aldo-profile.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;

9997ef491af2a79900a414d13d96a46d0f90ae8e
aldoibuado <aldo.ibuado@gmail.com>
2023-06-04 15:26:02 -0700

    adding styles to the header component, call to action buttons, image and social links

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import './header.css';
 import CTA from './CTA';
 import photo from '../../assets/Alita.png';
+import HeaderSocials from './HeaderSocials';
+import "./header.css";
 
 function Header() {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5>Hello I am</h5>
         <h1>Aldo Ibuado</h1>
         <h5 className="text-light">Fullstack Web Developer</h5>
         <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={photo} alt="" />
         </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )

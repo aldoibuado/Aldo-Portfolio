0824d1c201efff2dba49ee65ed54aca26dd963e2
aldoibuado <aldo.ibuado@gmail.com>
2023-06-05 15:49:53 -0700

    added styling to experience section

--- client/src/components/experience/Experience.jsx
+++ client/src/components/experience/Experience.jsx
@@ -14,29 +14,39 @@ function Experience() {
          <h3>FrontEnd Development</h3>
          <div className="experience__content">
           <article className="experience__details">
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
             <h4>HTML</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>CSS</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+           <div>
+           <h4>CSS</h4>
             <small className="text-light">Experienced</small>
+           </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>JavaScript</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+           <div>
+           <h4>JavaScript</h4>
             <small className="text-light">Experienced</small>
+           </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
             <h4>BootStrap</h4>
             <small className="text-light">Intermediate</small>
+            </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>React</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+           <div>
+           <h4>React</h4>
             <small className="text-light">Intermediate</small>
+           </div>
           </article>
          </div>
         </div>
@@ -45,29 +55,39 @@ function Experience() {
         <h3>BackEnd Development</h3>
          <div className="experience__content">
           <article className="experience__details">
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
             <h4>Node.js</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
             <h4>Express.js</h4>
             <small className="text-light">Experienced</small>
+            </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>Object-Oriented Programming (OOP)</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+           <div>
+           <h4>OOP</h4>
             <small className="text-light">Intermediate</small>
+           </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>Object-Relational Mapping (ORM)</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>ORM</h4>
             <small className="text-light">Intermediate</small>
+            </div>
           </article>
           <article className="experience__details">
-            <BsPatchCheckFill />
-            <h4>MERN stack</h4>
+            <BsPatchCheckFill className="experience__details-icon"/>
+           <div>
+           <h4>MERN stack</h4>
             <small className="text-light">Intermediate</small>
+           </div>
           </article>
          </div>
         </div>

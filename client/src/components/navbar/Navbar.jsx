b7af61ef2ed23f54b1a16862c8a8f79e14afdfde
aldoibuado <aldo.ibuado@gmail.com>
2023-06-05 10:02:33 -0700

    added useState hook to navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,15 +5,18 @@ import {FaBook} from 'react-icons/fa';
 import {BsFillBriefcaseFill} from 'react-icons/bs';
 import {AiFillMessage} from 'react-icons/ai';
 import './navbar.css';
+import {useState} from 'react';
 
 function Navbar() {
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
     <nav>
-      <a href="#"><AiTwotoneHome /></a>
-      <a href="#about"><AiOutlineUser /></a>
-      <a href="#experience"><FaBook /></a>
-      <a href="#portfolio"><BsFillBriefcaseFill /></a>
-      <a href="#contact"><AiFillMessage /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiTwotoneHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaBook /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsFillBriefcaseFill /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiFillMessage /></a>
     </nav>
   )
 }

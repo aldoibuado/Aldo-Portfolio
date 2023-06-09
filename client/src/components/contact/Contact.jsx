b8bd10b471b0c96a8f421927b192238f8feb5e65
aldoibuado <aldo.ibuado@gmail.com>
2023-06-09 16:34:05 -0700

    working on adding contact section

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {AiTwotoneMail} from 'react-icons/ai'
 import './contact.css';
 
 function contact() {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Contact Me</h5>
+      <h2>Reach Out</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiTwotoneMail />
+            <h4>Email</h4>
+            <h5>aldo.ibuado@gmail.com</h5>
+            <a href="mailto:aldo.ibuado@gmail.com" target="_blank" rel='noreferrer'>Send me a message</a>
+          </article>
+        </div>
+        {/* end of contact options */}
+        <form action="">
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" rows="7" placeholder="Enter Your Message" required />
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
 

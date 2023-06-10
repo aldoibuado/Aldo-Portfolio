ccabad3f25d61906db8b4e7d9e1b12126ac4b57c
aldoibuado <aldo.ibuado@gmail.com>
2023-06-09 17:17:15 -0700

    adding links to footer section

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {AiFillInstagram} from 'react-icons/ai';
 import './footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Aldo</a>
+
+      <ul className="permalinks">
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://instagram.com'><AiFillInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
